28d67f79a3e44bf28756ed49bd34142a5b4cbfb8
JangWoojun <woojun0107@naver.com>
2024-01-15 08:53:00 +0900

    feat: ChatAdapter 추가

---
+++ app/src/main/java/com/woojun/chatappui/ChatAdapter.kt
@@ -0,0 +1,39 @@
+package com.woojun.chatappui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.woojun.chatappui.databinding.ChatItemBinding
+
+class ChatAdapter(private val chatList: List<Chat>): RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+
+    class ChatViewHolder(private val binding: ChatItemBinding):
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(chat: Chat) {
+                binding.apply{
+
+                }
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        val binding = ChatItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
+        )
+        return ChatViewHolder(binding).also { handler ->
+            binding.apply {
+
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return chatList.size
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+        holder.bind(chatList[position])
+    }
+
+}
