2ef7cee7000eab4c71ee2c3a56ec11418b1baa2d
JangWoojun <woojun0107@naver.com>
2024-01-15 10:16:00 +0900

    feat: OtherViewHolder 추가 및 binding 세팅

--- app/src/main/java/com/woojun/chatappui/util/ChatAdapter.kt
+++ app/src/main/java/com/woojun/chatappui/util/ChatAdapter.kt
@@ -1,39 +1,81 @@
 package com.woojun.chatappui.util
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.woojun.chatappui.databinding.ChatItemBinding
+import com.woojun.chatappui.databinding.OtherChatItemBinding
 
-class ChatAdapter(private val chatList: List<Chat>): RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+class ChatAdapter(private val chatList: List<Chat>): RecyclerView.Adapter<ViewHolder>() {
 
-    class ChatViewHolder(private val binding: ChatItemBinding):
-        RecyclerView.ViewHolder(binding.root) {
-            fun bind(chat: Chat) {
-                binding.apply{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return when (viewType) {
+            1 -> {
+                val binding = ChatItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent, false
+                )
 
+                ChatViewHolder(binding).also { handler ->
+                    binding.apply {
+
+                    }
                 }
             }
-        }
+            else -> {
+                val binding = OtherChatItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent, false
+                )
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
-        val binding = ChatItemBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent, false
-        )
-        return ChatViewHolder(binding).also { handler ->
-            binding.apply {
+                OtherChatViewHolder(binding).also { handler ->
+                    binding.apply {
 
+                    }
+                }
             }
+
         }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (chatList[position].isUser) 1 else 0
     }
 
     override fun getItemCount(): Int {
         return chatList.size
     }
 
-    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-        holder.bind(chatList[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (chatList[position].isUser) {
+            (holder as ChatViewHolder).bind(chatList[position])
+        } else {
+            (holder as OtherChatViewHolder).bind(chatList[position])
+        }
+    }
+
+    class ChatViewHolder(private val binding: ChatItemBinding):
+        ViewHolder(binding.root) {
+        fun bind(chat: Chat) {
+            binding.apply{
+                messageText.text = chat.massage
+                dateText.text = chat.date
+                isShowText.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    class OtherChatViewHolder(private val binding: OtherChatItemBinding):
+        ViewHolder(binding.root) {
+        fun bind(chat: Chat) {
+            binding.apply{
+                messageText.text = chat.massage
+                dateText.text = chat.date
+                isShowText.visibility = View.INVISIBLE
+            }
+        }
     }
 
 }
